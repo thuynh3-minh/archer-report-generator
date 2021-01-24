8c21d3cbe9f1c7d7782b5ba829258ed3386d1c71
thuynh3-minh <65977877+thuynh3-minh@users.noreply.github.com>
2021-01-24 17:21:42 -0600

    Archer report with user interface using tkinter. It takes in three entries, has a browse button to select folder and a "create" button to run the archer generator code.
    Can be improved: add a pop up window when work is done,  add background, change the positions of buttons.
    However, this year my boss told me we will switch to another kind of report so I will work on that and maybe not continue on this one. I may still reuse the tkinter part.

--- archer.py
+++ archer.py
@@ -1,18 +1,11 @@
 import os
 import pandas as pd
 import pytesseract as tess
+from tkinter import filedialog
+from tkinter import *
+import tkinter as tk
 
-#date = '202010'
-date = input('Input the date in this form 202010 for Oct 2020: ')        
-#path = r'Q:\Information Security\Operations\Monthly Reporting\Archer\minh_test\@@2020 OCT'  
-path = input('input the folder file')
-
-snow_count = int(input("Enter the snow count : ") or "0")
-#snow_count = input("type in the snow count")
-
-malware_people_count = int(input("Enter the the number of malware cleaned by people : ") or "0")
-#malware_people_count = input("type in the number of malware cleaned by people")
-
+#function to return the link for each file
 def find_link(name, path):
     try:
         for root, dirs, files in os.walk(path):
@@ -33,6 +26,7 @@ def count_proofpoint(date,proofpoint_link):
         return int(count/2)
     except:
         return 0
+    
 #count the scep number
 def count_scep(incident_link):
     try:
@@ -56,6 +50,7 @@ def count_backdraft(html_link):
 def count_tap(picture_link):
     try:
         #picture = 'threat_campaigns_vs_individual_threats-2020_10_01-2020_10_31.png'
+        #location of the pytesseract.exe in the computer. Here you have to adjust it depending on where it is stored in your pc
         tess.pytesseract.tesseract_cmd = r'Q:\Information Security\Operations\Monthly Reporting\Archer\minh_test\Tesseract-OCR\tesseract.exe'
         from PIL import Image
         img = Image.open(picture_link)
@@ -65,10 +60,8 @@ def count_tap(picture_link):
         return tap_count
     except:
         return 0
-
-#small functions to count website
-malwarebytes_link = find_link('Malwarebytes', path)
-mwb = pd.read_csv(malwarebytes_link)
+    
+#small functions to support the website_count and malware_count line 9-10 in the oldmain aka report function. Count numbers in the excel file
 def web(a):
     if('Website' in a):
         return True
@@ -80,7 +73,18 @@ def malware(a):
     else:
         return False
 
-def main():
+#main function takes three user inputs from the UI. It takes date, path and number of snow emails and malware emails removed by people in that order
+def report(input1,input2,input3,folder_path):
+
+    date = input1
+    path = folder_path
+    snow_count = input2
+    malware_people_count = input3
+
+    #small functions to count website
+    malwarebytes_link = find_link('Malwarebytes', path)
+    mwb = pd.read_csv(malwarebytes_link)
+    
     website_count = sum(mwb['Category'].apply(lambda x: web(x)))
     malware_count = sum(mwb['Category'].apply(lambda x: malware(x)))
     
@@ -106,14 +110,69 @@ def main():
 
     # Gather information
     archer = pd.DataFrame(data = value, index = label, columns = ['Archer report'])
-    print(archer)
-    #write to an excel file
+    #print(archer)
+    
+    #write to an excel file in the old directory
     archer.to_excel(path + '\\' + "my_archer_report.xlsx")
+    
+    #can be improve: create a pop up window to inform success.
     print("It is done! Please check the file my_archer_report.xlsx in the directory you entered. Thank you!")
     
+
+#basic interface of the app
+root = tk.Tk()
+heightApp=500
+widthApp=500
+root.geometry(str(heightApp)+"x"+str(widthApp))
+root.title("Archer report creator")
+root.iconbitmap(r'Q:\Information Security\Operations\Monthly Reporting\Archer\minh_test\archer\archericon.ico')
+canvas=tk.Canvas(root, height=heightApp, width=widthApp)
+canvas.pack()
+
+#function support the browse button
+def browse_button():
+    # Allow user to select a directory and store it in global var
+    # called folder_path
+    global folder_path
+    filename = filedialog.askdirectory()
+    folder_path.set(filename)
+    #print(filename)
     
+#a test function to test every button and entry. Activate when button create report is clicked. Now it is replaced by the report function 
+def test_function(entry1,entry2,entry3,folder_path):
+    entry2=int(entry2 or 0)
+    entry3=int(entry3 or 0)
+    print(type(entry1))
+    print(type(entry2))
+    print(type(entry3))
+    print(type(folder_path))
 
-if __name__ == "__main__":
-    main()
+frame=tk.Frame(root,bg="#d3d3d3",bd=5)#bg=#
+frame.place(relx=0.5,rely=0,relwidth=0.75,relheight=0.6,anchor='n')
 
-#pip install pytesseract, xlrd, pandas, image, lxml, openpyxl or python -m pip install
+#date entry 
+label1 = tk.Label(frame,font=30,text="Enter the date in form YYYYMM:",bg="#d3d3d3").place(relx=0,rely=0.10,relwidth=0.65,relheight=0.1)
+entry1 = tk.Entry(frame,font=30)
+entry1.place(relx=0,rely=0.20,relwidth=0.65,relheight=0.1)
+
+#snowcount entry
+label2 = tk.Label(frame,font=30,text="Enter the snow count",bg="#d3d3d3").place(relx=0,rely=0.30,relwidth=0.65,relheight=0.1)
+entry2 = tk.Entry(frame,font=30)
+entry2.place(relx=0,rely=0.40,relwidth=0.65,relheight=0.1)
+
+#malwarebytes cleaned by people entry
+label3 = tk.Label(frame,font=30,text="Enter the Malwarebytes",bg="#d3d3d3").place(relx=0,rely=0.50,relwidth=0.65,relheight=0.1)
+entry3 = tk.Entry(frame,font=30)
+entry3.place(relx=0,rely=0.60,relwidth=0.65,relheight=0.1)
+
+#browse button and path
+folder_path = StringVar()
+entry4 = tk.Label(frame,textvariable=folder_path).place(relx=0, rely=0.9,relwidth=0.65,relheight=0.1)
+label_file_explorer = tk.Label(frame,text = "Folder location for archer",font=30,bg="#d3d3d3").place(relx=0, rely=0.7,relwidth=0.65,relheight=0.1) 
+button2 = tk.Button(frame,text="Browse", command=browse_button,bg="#d3d3d3").place(relx=0, rely=0.8,relwidth=0.65,relheight=0.1)
+
+#create report button
+button = tk.Button(frame,text="Create report", font=30, command=lambda: report(entry1.get(),entry2.get(),entry3.get(),folder_path.get()))
+button.place(relx=0.7,rely=0.2,relwidth=0.3,relheight=0.1)
+
+root.mainloop()
